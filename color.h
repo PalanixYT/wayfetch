5dfc6d5cb3fdbee8e7af04416804ffc02a2b3eb4
Palanix <davidospanov@gmail.com>
2021-06-08 21:08:49 +0200

    moved COLORS here to allow for colorful ascii art

---
+++ color.h
@@ -0,0 +1,11 @@
+//here are the colors defined, to allow for multicolor logos
+#define BLACK "\033[1;30m"
+#define RED  "\033[1;31m"
+#define GREEN  "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE  "\033[1;34m"
+#define MAGENTA  "\033[1;35m"
+#define CYAN  "\033[1;36m"
+#define WHITE  "\033[1;37m"
+
+#define CLOSE "\033[0m"
