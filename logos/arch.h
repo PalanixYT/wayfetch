703e57b46a4428577eaf613150350a068aac606d
Palanix <davidospanov@gmail.com>
2021-06-08 21:09:26 +0200

    adjusted for previous commit

--- logos/arch.h
+++ logos/arch.h
@@ -1,24 +1,25 @@
+#include "../color.h"
 #define COLOR CYAN
 #define ROWS 19
 char *logo[] = {
-    "                  -`                    ",
-    "                 .o+`                   ",
-    "                `ooo/                   ",
-    "               `+oooo:                  ",
-    "              `+oooooo:                 ",
-    "              -+oooooo+:                ",
-    "            `/:-:++oooo+:               ",
-    "           `/++++/+++++++:              ",
-    "          `/++++++++++++++:             ",
-    "         `/+++ooooooooooooo/`           ",
-    "        ./ooosssso++osssssso+`          ",
-    "       .oossssso-````/ossssss+`         ",
-    "      -osssssso.      :ssssssso.        ",
-    "     :osssssss/        osssso+++.       ",
-    "    /ossssssss/        +ssssooo/-       ",
-    "  `/ossssso+/:-        -:/+osssso+-     ",
-    " `+sso+:-`                 `.-/+oso:    ",
-    "`++:.                           `-/+/   ",
-    ".`                                 `/   "
+    "                  -`                 ",
+    "                 .o+`                ",
+    "                `ooo/                ",
+    "               `+oooo:               ",
+    "              `+oooooo:              ",
+    "              -+oooooo+:             ",
+    "            `/:-:++oooo+:            ",
+    "           `/++++/+++++++:           ",
+    "          `/++++++++++++++:          ",
+    "         `/+++ooooooooooooo/`        ",
+    "        ./ooosssso++osssssso+`       ",
+    "       .oossssso-````/ossssss+`      ",
+    "      -osssssso.      :ssssssso.     ",
+    "     :osssssss/        osssso+++.    ",
+    "    /ossssssss/        +ssssooo/-    ",
+    "  `/ossssso+/:-        -:/+osssso+-  ",
+    " `+sso+:-`                 `.-/+oso: ",
+    "`++:.                           `-/+/",
+    ".`                                 `/"
 };
 
