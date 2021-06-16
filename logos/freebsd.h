74c72c5ae00f8ad4e35cb246c26de7462dc97e8a
Biel Sala <bielsalamimo@gmail.com>
2021-06-16 22:01:12 +0200

    Add FreeBSD logo

---
+++ logos/freebsd.h
@@ -0,0 +1,23 @@
+#include "../color.h"
+#define COLOR WHITE
+#define ROWS 15
+char *logo[] = {
+    "```                        "RED"`"CLOSE"    ",
+    "  ` `.....---..."RED"....--.```   -/ "CLOSE"",
+    "  +o   .--`         "RED"/y:`      +."CLOSE"",
+    "   yo`:.            "RED":o      `+- "CLOSE"",
+    "    y/               "RED"-/`   -o/  "CLOSE"",
+    "   .-                  "RED"::/sy+:. "CLOSE"",
+    "   /                     "RED"`--  / "CLOSE"",
+    "  `:                          "RED":`"CLOSE"",
+    "  `:                          "RED":`"CLOSE"",
+    "   /                          "RED"/ "CLOSE"",
+    "   .-                        "RED"-. "CLOSE"",
+    "    --                      "RED"-.  "CLOSE"",
+    "     `:`                  "RED"`:`   "CLOSE"",
+    "       "RED".--             `--.     "CLOSE"",
+    "          "RED".---.....----.        "CLOSE"",
+    "                                ",
+};
+//IMPORTANT: Always make the lines the same length. There shouldn't be any extra spaces, as they are added later on by the programm itself
+//IMPORTANT: the last line should be only spaces with the same length as the other lines, ROWS should exclude the last line
