7eff6dd171dc65f977537652d93d96b0bf5709ac
Biel Sala <bielsalamimo@gmail.com>
2021-06-16 21:26:35 +0200

    Add Devuan logo

---
+++ logos/devuan.h
@@ -0,0 +1,27 @@
+#include "../color.h"
+#define COLOR MAGENTA
+#define ROWS 19
+char *logo[] = {
+    "   ..,,;;;::;,..                ",
+    "           `':ddd;:,.           ",
+    "                 `'dPPd:,.      ",
+    "                     `:b$$b`.   ",
+    "                        'P$$$d` ",
+    "                         .$$$$$`",
+    "                         ;$$$$$P",
+    "                      .:P$$$$$$`",
+    "                  .,:b$$$$$$$;' ",
+    "             .,:dP$$$$$$$$b:'   ",
+    "      .,:;db$$$$$$$$$$Pd'`      ",
+    " ,db$$$$$$$$$$$$$$b:'`          ",
+    ":$$$$$$$$$$$$b:'`               ",
+    " `$$$$$bd:''`                   ",
+    "   `'''`                        ",
+    "                                ",
+    "                                ",
+    "                                ",
+    "                                ",
+    "                                ",
+};
+//IMPORTANT: Always make the lines the same length. There shouldn't be any extra spaces, as they are added later on by the programm itself
+//IMPORTANT: the last line should be only spaces with the same length as the other lines, ROWS should exclude the last line
