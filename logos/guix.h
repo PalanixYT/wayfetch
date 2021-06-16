de16bf92c19e22a25341c531bc66f63d438df958
Biel Sala <bielsalamimo@gmail.com>
2021-06-16 22:07:45 +0200

    Add Guix logo

---
+++ logos/guix.h
@@ -0,0 +1,18 @@
+#include "../color.h"
+#define COLOR YELLOW
+#define ROWS 10
+char *logo[] = {
+    " ..                             `.",
+    " `--..```..`           `..```..--`",
+    "   .-:///-:::.       `-:::///:-.  ",
+    "      ````.:::`     `:::.````     ",
+    "           -//:`    -::-          ",
+    "            ://:   -::-           ",
+    "            `///- .:::`           ",
+    "             -+++-:::.            ",
+    "              :+/:::-             ",
+    "              `-....`             ",
+    "                                  ",
+};
+//IMPORTANT: Always make the lines the same length. There shouldn't be any extra spaces, as they are added later on by the programm itself
+//IMPORTANT: the last line should be only spaces with the same length as the other lines, ROWS should exclude the last line
