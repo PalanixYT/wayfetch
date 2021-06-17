3221477d7cd0b3a081d3ca02dfd0b8c83d3a1f97
Biel Sala <bielsalamimo@gmail.com>
2021-06-17 15:18:12 +0200

    Add Trisquel logo

---
+++ logos/trisquel.h
@@ -0,0 +1,27 @@
+#include "../color.h"
+#define COLOR BLUE
+#define ROWS 19
+char *logo[] = {
+        "                         ______     ",
+        "                      ___________   ",
+        "      ______         _____   _____  ",
+        "   ____________     _____   __ ____ ",
+        " ______   ______     ____   __   ___",
+        "____   ___   _____    _______   ____",
+        "___   ______  ______     __   _____ ",
+        "____      ___  _________  ________  ",
+        " _____   ____  _____________"CYAN"_____   "CLOSE,
+        "  __________    _______"CYAN"______       "CLOSE,
+        CYAN"    _______     ________            ",
+        CYAN"               _______   ____       ",
+        CYAN"              ______   ________     ",
+        CYAN"              _____   ____  ____    ",
+        CYAN"               _____   _____  ___   ",
+        CYAN"                ______   __  ___    ",
+        CYAN"                  _____________     ",
+        CYAN"                     _________      ",
+        "                                    ",
+
+};
+//IMPORTANT: Always make the lines the same length. There shouldn't be any extra spaces, as they are added later on by the programm itself
+//IMPORTANT: the last line should be only spaces with the same length as the other lines, ROWS should exclude the last line
