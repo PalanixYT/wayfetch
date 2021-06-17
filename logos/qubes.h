8e1a281cc98a9b6296e21f216222622ccf09c0ab
Palanix <davidospanov@gmail.com>
2021-06-17 17:52:45 +0200

    just made a few changes, mainly fixes

--- logos/qubes.h
+++ logos/qubes.h
@@ -1,28 +1,29 @@
 #include "../color.h"
-#define COLOR BLUE
+#define COLOR MAGENTA
+#define NAMECOLOR BLUE
 #define ROWS 21
 char *logo[] = {
-        "               `..--..`                ",
-        "            `.----------.`             ",
-        "        `..----------------..`         ",
-        "     `.------------------------.``     ",
-        " `..-------------....-------------..`  ",
-        ".::----------..``    ``..----------:+: ",
-        ":////:----..`            `..---:/ossso ",
-        ":///////:`                  `/osssssso ",
-        ":///////:                    /ssssssso ",
-        ":///////:                    /ssssssso ",
-        ":///////:                    /ssssssso ",
-        ":///////:                    /ssssssso ",
-        ":///////:                    /ssssssso ",
-        ":////////-`                .:sssssssso ",
-        ":///////////-.`        `-/osssssssssso ",
-        "`//////////////:-```.:+ssssssssssssso- ",
-        "  .-://////////////sssssssssssssso/-`  ",
-        "     `.:///////////sssssssssssssso:.   ",
-        "         .-:///////ssssssssssssssssss/`",
-        "            `.:////ssss+/+ssssssssssss.",
-        "                `--//-    `-/osssso/.  ",
+BLUE     "               `..--..`                "CLOSE,
+BLUE     "            `.----------.`             "CLOSE,
+BLUE     "        `..----------------..`         "CLOSE,
+BLUE     "     `.------------------------.``     "CLOSE,
+BLUE     " `..-------------....-------------..`  "CLOSE,
+BLUE     ".::----------..``    ``..----------:+: "CLOSE,
+BLUE     ":////:----..`            `..---:/ossso "CLOSE,
+BLUE     ":///////:`                  `/osssssso "CLOSE,
+BLUE     ":///////:                    /ssssssso "CLOSE,
+BLUE     ":///////:                    /ssssssso "CLOSE,
+BLUE     ":///////:                    /ssssssso "CLOSE,
+BLUE     ":///////:                    /ssssssso "CLOSE,
+BLUE     ":///////:                    /ssssssso "CLOSE,
+BLUE     ":////////-`                .:sssssssso "CLOSE,
+BLUE     ":///////////-.`        `-/osssssssssso "CLOSE,
+BLUE     "`//////////////:-```.:+ssssssssssssso- "CLOSE,
+BLUE     "  .-://////////////sssssssssssssso/-`  "CLOSE,
+BLUE     "     `.:///////////sssssssssssssso:.   "CLOSE,
+BLUE     "         .-:///////ssssssssssssssssss/`"CLOSE,
+BLUE     "            `.:////ssss+/+ssssssssssss."CLOSE,
+BLUE     "                `--//-    `-/osssso/.  "CLOSE,
         "                                       "
 };
 //IMPORTANT: Always make the lines the same length. There shouldn't be any extra spaces, as they are added later on by the programm itself
