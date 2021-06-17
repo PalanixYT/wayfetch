13b5627546ae7d9c179ca520afe574fa960a02a6
Biel Sala <bielsalamimo@gmail.com>
2021-06-17 15:17:59 +0200

    Add OpenSUSE logo

---
+++ logos/opensuse.h
@@ -0,0 +1,32 @@
+#include "../color.h"
+#define COLOR GREEN
+#define ROWS 19
+char *logo[] = {
+        WHITE"           .;ldkO0000Okdl;.           "CLOSE,
+        WHITE"       .;d00xl:^''''''^:ok00d;.       "CLOSE,
+        WHITE"     .d00l'                'o00d.     "CLOSE,
+        WHITE"   .d0Kd'  "GREEN"Okxol:;,."WHITE"          :O0d.   "CLOSE,
+        WHITE"  .OK"GREEN"KKK0kOKKKKKKKKKKOxo:,"WHITE"      lKO.  "CLOSE,
+        WHITE" ,0K"GREEN"KKKKKKKKKKKKKKK0P^"WHITE",,,"GREEN"^dx:"WHITE"    ;00, "CLOSE,
+        WHITE".OK"GREEN"KKKKKKKKKKKKKKKk'"WHITE".oOPPb."GREEN"'0k."WHITE"   cKO."CLOSE,
+        WHITE":KK"GREEN"KKKKKKKKKKKKKKK: "WHITE"kKx..dd"GREEN" lKd"WHITE"   'OK:"CLOSE,
+        WHITE"dKK"GREEN"KKKKKKKKKOx0KKKd "WHITE"^0KKKO'"GREEN" kKKc"WHITE"   dKd"CLOSE,
+        WHITE"dKK"GREEN"KKKKKKKKKK;.;oOKx,.."WHITE"^"GREEN"..;kKKK0.  "WHITE"dKd"CLOSE,
+        WHITE":KK"GREEN"KKKKKKKKKK0o;...^cdxxOK0O/^^'  "WHITE".0K:"CLOSE,
+        WHITE" kKK"GREEN"KKKKKKKKKKKKK0x;,,......,;od  "WHITE"lKk "CLOSE,
+        WHITE" '0K"GREEN"KKKKKKKKKKKKKKKKKKKK00KKOo^  "WHITE"c00' "CLOSE,
+        WHITE"  'kK"GREEN"KKOxddxkOO00000Okxoc;''   "WHITE".dKk'  "CLOSE,
+        WHITE"    l0Ko.                    .c00l'   "CLOSE,
+        WHITE"     'l0Kk:.              .;xK0l'     "CLOSE,
+        WHITE"        'lkK0xl:;,,,,;:ldO0kl'        "CLOSE,
+        WHITE"            '^:ldxkkkkxdl:^'          "CLOSE,
+        WHITE"                                      "CLOSE,
+
+};
+//IMPORTANT: Always make the lines the same length. There shouldn't be any extra spaces, as they are added later on by the programm itself
+//IMPORTANT: the last line should be only spaces with the same length as the other lines, ROWS should exclude the last line
+
+
+
+
+
