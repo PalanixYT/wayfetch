f8b79697c1ec915e2c3d9345c5b772ef53112078
Palanix <davidospanov@gmail.com>
2021-07-12 16:46:47 +0200

    removed old references that don't exist anymore, moved around some lines

--- config.pre.h
+++ config.pre.h
@@ -1,8 +1,7 @@
 #include "logos/tux.h"
 //here you can define which logo wayfetch is going to use. See logos folder for available options
+//
 #define PACDIR "/var/lib/pacman/local" // here are all packages incase of pacman
-#define ELEMENTS 19
-//this is how many elements are to be printed in the info
 
 // SEPARATOR is the string defined that wayfetch will print for every factor
 #define SEPARATOR ":" // Example: "OS: Parabola x86_64"
@@ -10,13 +9,10 @@
 // #define SEPARATOR "->" // Example: "OS -> Parabola x86_64"
 // #define SEPARATOR "="  // Example: "OS = Parabola x86_64"
 
-//usually color is defined by the logo
-//#define COLOR
-// BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
-
+#define ELEMENTS 19
+//this is how many elements are to be printed in the info
 
 //here comes the order of elements
-// info[0] and info[1] are reserved for the title and bar
 void order() {
 				hname();
 				os();
@@ -38,3 +34,8 @@ void order() {
 				get_colors();
 				get_colors2();
 }
+
+
+//usually color is defined by the logo
+//#define COLOR
+// BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
